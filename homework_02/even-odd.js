45e07345eacd02b059d8738bfe965b04e0b9366f
AnhNghia Nguyen <nghianghesi@gmail.com>
2019-04-03 03:12:30 +0700

    async filter

--- homework_02/even-odd.js
+++ homework_02/even-odd.js
@@ -1,3 +1,25 @@
+
+Array.prototype.asyncFilter = function(filterfunc){
+    return new Promise((resolve)=>{      
+        const result=[];
+        let idx = 0;
+        let self = this;
+        let dofilter = () => {
+            //console.log(idx);
+            if(idx < self.length){
+                if(filterfunc(self[idx])){
+                    result.push(self[idx]);
+                }
+                idx++;
+                setImmediate(dofilter);
+            }else{
+                resolve(result);        
+            }
+        };
+        dofilter();
+    });
+}
+
 Array.prototype.even = function(){
     return new Promise((resolve)=>{        
         resolve(this.filter((x) => x % 2 == 0));
@@ -11,6 +33,15 @@ Array.prototype.odd = function(){
 }
 
 console.log('start');
+let A = [], B=[];
+for(let i=0;i<100000;i++){
+    A.push(i);
+    B.push(i);
+    B.push(i*2);
+}
+B.asyncFilter(x=>x%2==1).then(() => console.log ('done odd'));
+A.asyncFilter(x=>x%2==0).then(() => console.log ('done even'));
+
 [1,2,3,4,5,6,7,8].even().then((result) => {console.log(result);});
 [1,2,3,4,5,6,7,8].odd().then((result) => {console.log(result);});
 console.log('end');
--- homework_02/event-loop-test.js
+++ homework_02/event-loop-test.js
@@ -1,7 +1,13 @@
 setTimeout(()=>{console.log('timeout');setImmediate(()=>{console.log('immedidate 2')});},0);
 setTimeout(()=>{console.log('timeout 100');},100);
 setTimeout(()=>{console.log('timeout 2');},2);
-setImmediate(()=>{console.log('immedidate')});
+setImmediate(()=>{
+    console.log('immedidate');
+    queueMicrotask(()=>{console.log('micro task 2');});
+    setImmediate(()=>{
+        console.log('immedidate 3');
+    });    
+});
 new Promise(function(resolve, reject){setTimeout(()=>{resolve('promise-timeout');},0);})
 .then((res)=>{console.log(res);});
 new Promise(function(resolve, reject){setImmediate(()=>{resolve('promise-immediate');},0);})
