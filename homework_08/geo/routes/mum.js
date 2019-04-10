a3dedbb5b70086baa52c6bef44e28e3e3a6e8345
AnhNghia Nguyen <nghianghesi@gmail.com>
2019-04-10 10:04:04 +0700

    geo search places

--- homework_08/geo/routes/mum.js
+++ homework_08/geo/routes/mum.js
@@ -3,13 +3,14 @@ const express = require('express');
 
 const client = new MongoClient('mongodb://localhost:27017');
 const router = express.Router();
-const collectionname = 'fairfieldlocations';
+const collectionname = 'places';
 
-router.all((req, res, next)=>{
+router.use((req, res, next)=>{
     client.connect((err)=>{
         if(!err){
-            req.db = client.db('homework07');        
-            reg.db.collection(collectionname);
+            req.db = client.db('homework08');        
+            req.db.collection(collectionname);
+            next();
         }else{
             next('database error');
         }
@@ -20,16 +21,21 @@ router.post('/',(req, res, next) =>{
     let query = {category:req.body.category, 
                 location:{'$near':{'$geometry':{
                                         type:'Point',
-                                        coordinates:[41.017654,-91.9665342]},
+                                        coordinates:[-91.9665342,41.017654]},
                                     '$maxDistance':2000}}};
     if(req.body.search){
         query.name=`/${req.body.search}/`;
     }
-    this.db.collection(collectionname).find(query,{_id:0},(err, result)=>{
+    console.log(query);
+
+    req.db.collection(collectionname).find(query).project({_id:0}).toArray((err, result)=>{
         if(!err){
-            res.status(200).json(result.ops);
+            console.log(result);
+            res.status(200).json(result);
         }else{
             res.status(500).json('database error');
         }
     });
 });
+
+module.exports =router;
